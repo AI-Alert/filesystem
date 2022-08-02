15a2b6674088aef00bf74ae166f4c82e5d561203
tom <veniamin.b01@gmail.com>
2022-08-02 12:11:51 +0600

    Енв в гитигнор, добавил createdAT к каждому полю в схеме MongoDB, сделал ожидание подключения к базе

--- server/models/File.js
+++ server/models/File.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const fileSchema = new mongoose.Schema({
-    name: {type: String, required: true, unique: true},
-    mime_type: {type: String, required: true},
-    size: {type: Number, required: true},
+    name: {type: String, required: true, unique: true, createdAt: new Date()},
+    mime_type: {type: String, required: true, createdAt: new Date()},
+    size: {type: Number, required: true, createdAt: new Date()},
 });
 
 module.exports = mongoose.model("fileSchema", fileSchema);
